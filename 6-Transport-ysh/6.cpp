3c0fe515584310f9c20b77a77be6224be51c367b
Skye <skyzhouch@outlook.com>
2022-12-17 21:41:26 +0800

    Final edit

--- 6-Transport-ysh/6.cpp
+++ 6-Transport-ysh/6.cpp
@@ -1,10 +1,11 @@
 ﻿#include <iostream>
 #include <vector>
 #include <map>
+#include <stack>
 using namespace std;
 
 #define MAX_STORE_NUM 3
-#define MAX_TRANSPORT_END_NUM 8
+#define MAX_TRANSPORT_END_NUM 5
 namespace YshTransport {
 	/**********************************************************************************
 	工厂仓库:工厂完成生产后，货物存储在仓库中，等待运输
@@ -22,15 +23,136 @@ namespace YshTransport {
 
 	};
 
+	struct TransportEdge {//存储网表的边的信息
+		TransportEdge(const int& d = 0) { dest = d; link = NULL; }
+		int dest;
+		TransportEdge* link;
+	};
+	/**********************************************************************************
+	迭代器
+	***********************************************************************************/
+	template <typename T, typename U>
+	class Iterator {
+	public:
+		typedef typename std::vector<T>::iterator iter_type;
+		Iterator(U* p_data, bool reverse = false) : m_p_data_(p_data) {
+			m_it_ = m_p_data_->begin();
+		}
+		void First() {
+			m_it_ = m_p_data_->begin();
+		}
+		void Next() {
+			m_it_ = m_p_data_->next(m_it_);
+		}
+		bool IsDone() {
+			return (m_it_ == m_p_data_->end());
+		}
+		iter_type Current() {
+			return m_it_;
+		}
+	private:
+		U* m_p_data_;	
+		iter_type m_it_;
+	};
 	/***********************************************************************************
 	物流网点:货物运输从仓库运输至物流网点
 	************************************************************************************/
 	class TransportEnd {
 	public:
-		TransportEnd(const int& i) :id(i) {};//初始化
-		int getId() { return id; }//返回ID
+		TransportEnd(const int& i, const int& s = 0) :id(i), storage(s) { adj = NULL; };//初始化
+		int getId()const { return id; }//返回ID
+		TransportEdge* getFirstEdge()const { return adj; }
+		void addStorage(const int& s) { storage += s; }//添加库存
+		void addEdge(const int& d) {//添加新的边
+			TransportEdge* p = adj;
+			if (adj == NULL) {
+				p = new TransportEdge(d);
+				adj = p;
+				return;
+			}
+			while (p->dest != d && p->link != NULL)p = p->link;
+			if (p->dest == d)return;
+			else if (p->link == NULL) {
+				p->link = new TransportEdge(d);
+				p = p->link;
+				p->dest = d;
+			}
+			return;
+		}
 	private:
 		int id;//网点编号
+		int storage;//网点库存
+		TransportEdge* adj;//边的头指针
+	};
+	/***********************************************************************************
+	物流网:存储记录整个物流网的联通状况
+	************************************************************************************/
+	class TransportEndGraph {
+	public:
+		TransportEndGraph() {};//初始化
+		TransportEnd getValue(int id) const {
+			for (TransportEnd i : table) {
+				if (i.getId() == id)return i;
+			}
+		}
+		vector<TransportEnd>::iterator begin() { vector<TransportEnd>::iterator tmp = table.begin(); return tmp; }
+		vector<TransportEnd>::iterator end() { vector<TransportEnd>::iterator tmp = table.end(); return tmp; }
+		vector<TransportEnd>::iterator next(vector<TransportEnd>::iterator it) { 
+			vector<TransportEnd>::iterator tmp = table.begin();
+			for (; tmp != table.end(); tmp++) {
+				if (tmp->getId() == it->getId()) {
+					tmp++;
+					return tmp;
+				}
+			}
+		}
+		int size()const { return table.size(); }	//返回当前顶点数
+		bool insertVertex(const TransportEnd& vertex) {	//在图中插入一个顶点vertex
+			table.push_back(vertex);
+			return true;
+		}
+		bool insertEdge(const int& srcId, const int& endId) {	//在图中插入一条边(v1,v2)
+			for (int i = 0; i < table.size(); i++) {
+				if (table.at(i).getId() == srcId) {
+					table.at(i).addEdge(endId);
+					return true;
+				}
+			}
+			return false;
+		}
+		void iterate(const int& id) {//遍历图，从初始点id开始遍历图
+			stack<int> ste;
+			vector<int> flag;
+			TransportEdge* ptE;
+			int tmp = findId(id);
+			for (int i = 0; i < table.size(); i++)flag.push_back(0);
+			ste.push(tmp);
+			while (!ste.empty()) {
+				tmp = ste.top();
+				cout << table.at(tmp).getId() << endl;
+				ste.pop();
+				flag.at(tmp) = 1;
+				ptE = table.at(tmp).getFirstEdge();
+				while (ptE != NULL) {
+					tmp = findId(ptE->dest);
+					if (flag.at(tmp) == 0) {
+						ste.push(tmp);
+					}
+					ptE = ptE->link;
+				}
+			}
+		}
+		Iterator<TransportEnd, TransportEndGraph>* CreateIterator() {
+			return new Iterator<TransportEnd, TransportEndGraph>(this);
+		}
+	private:
+		vector<TransportEnd> table;//存储图的邻接表结构
+		int findId(const int& id)const {//根据id找到再vector中的位置
+			for (int i = 0; i < table.size(); i++) {
+				if (table.at(i).getId() == id)return i;
+			}
+		}
+		friend class TransportEndGraphIterator;
 	};
 
 	struct route {//物流路线结构体
@@ -59,7 +181,7 @@ namespace YshTransport {
 	};
 
 	/***********************************************************************************
-	顾问接口类
+	顾问接口类，可供main函数调用
 	************************************************************************************/
 	class Client {
 	public:
@@ -101,13 +223,17 @@ namespace YshTransport {
 				cout << "库存容量：" << i.getStorgae() << "   " << endl;
 			}
 		}
-		static void show_transportEnd(const vector<TransportEnd>& tE) {
-			cout << "现有物流网点" << tE.size() << "个，编号分别为" << endl;
-			for (TransportEnd i : tE) { cout << i.getId() << endl; }
-		}
 		static void show_transportCom(const vector<TransportCompany>& tC) {
 			cout << "现有物流公司" << tC.size() << "个，分别为：" << endl;
 			for (TransportCompany i : tC) { cout << i.getId() << " " << i.getName() << endl; }
+		}
+		static void show_transportEnd(TransportEndGraph tEG) {
+			cout << "现有物流网点" << tEG.size() << "个，编号分别为" << endl;
+			//tEG.iterate(1);//从编号为1开始遍历所有节点
+			Iterator<TransportEnd, TransportEndGraph>* it = tEG.CreateIterator();//利用迭代器遍历图
+			for (it->First(); !it->IsDone(); it->Next()) {
+				cout << (*it->Current()).getId() << endl;
+			}
 		}
 		static void add_storage(vector<FactoryStore>& fs) {
 			show_factoryStore(fs);		//展示仓库
@@ -127,7 +253,7 @@ namespace YshTransport {
 			cout << "按回车键继续" << endl;
 			while (getchar() != '\n') {}
 		}
-		static void trans_storage(vector<FactoryStore>& fs, vector<TransportEnd> te, vector<TransportCompany> tc) {
+		static void trans_storage(vector<FactoryStore>& fs, TransportEndGraph& teg, vector<TransportCompany>& tc) {
 			//选择仓库和发货数量
 			show_factoryStore(fs);//展示仓库
 			int storeId, storeNum;
@@ -140,11 +266,11 @@ namespace YshTransport {
 			cout << "请输入发货的数量：";
 			inputInt(storeNum, ifs->getStorgae());
 
-			//选择收获网点
-			show_transportEnd(te);//展示网点信息
+			//选择收货网点
+			show_transportEnd(teg);//展示网点信息
 			int endId;
 			cout << "请输入要收货的网点编号：";
-			inputInt(endId, te.size());
+			inputInt(endId, teg.size());
 
 			//选择物流公司
 			show_transportCom(tc);//展示物流公司信息	
@@ -171,6 +297,7 @@ namespace YshTransport {
 			}
 			//发货
 			ifs->moveStorage(storeNum);
+
 			cout << "发货成功！！" << endl;
 			cout << "按回车键继续" << endl;
 			while (getchar() != '\n') {}
@@ -185,8 +312,12 @@ int main()
 	vector<YshTransport::FactoryStore> fStores;
 	for (int i = 1; i <= MAX_STORE_NUM; i++) { YshTransport::FactoryStore tmp(i, 5000 - i * 1000); fStores.push_back(tmp); }
 	//初始化物流网点
-	vector<YshTransport::TransportEnd> tsEnds;
-	for (int i = 1; i <= MAX_TRANSPORT_END_NUM; i++) { YshTransport::TransportEnd tmp(i); tsEnds.push_back(tmp); }
+	YshTransport::TransportEndGraph tsEndGraph;
+	for (int i = 1; i <= MAX_TRANSPORT_END_NUM; i++) { tsEndGraph.insertVertex(YshTransport::TransportEnd(i)); }
+	tsEndGraph.insertEdge(1, 2);
+	tsEndGraph.insertEdge(1, 3);
+	tsEndGraph.insertEdge(1, 5);
+	tsEndGraph.insertEdge(3, 4);
 	//初始化物流公司
 	vector<YshTransport::TransportCompany> tsCompys;
 	string tmp = "ZT";
@@ -211,7 +342,7 @@ int main()
 				YshTransport::Client::add_storage(fStores);
 				break;
 			case 2:	//发货
-				YshTransport::Client::trans_storage(fStores, tsEnds, tsCompys);
+				YshTransport::Client::trans_storage(fStores, tsEndGraph, tsCompys);
 				break;
 			case 0:
 				exit(0);
